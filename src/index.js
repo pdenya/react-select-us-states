b97f2e5931004d36c7c84b5637acc29969b15f9d
Paul Denya <pdenya@gmail.com>
2021-10-20 14:17:33 -0400

    build fix and build

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ import states from "./states.json";
 import PropTypes from "prop-types";
 
 class SelectUSState extends React.Component {
-  handleChange = event => {
-    this.props.onChange && this.props.onChange(event.currentTarget.value, event.currentTarget.name || event.currentTarget.id);
-  };
-
   render() {
     const { id, className } = this.props;
     return (
-      <select id={id} className={className} onChange={this.handleChange} value={this.props.value} name={this.props.name}>
-        { this.props.placeholder && <option value="">{this.props.placeholder}<option> }
+      <select
+        id={id}
+        className={className}
+        onChange={e => this.props.onChange && this.props.onChange(e.currentTarget.value, e.currentTarget.name)}
+        value={this.props.value}
+        name={this.props.name}>
+
+        { this.props.placeholder && <option value="">{this.props.placeholder}</option> }
+
         {states.map(item => (
           <option key={item.abbreviation} value={item.abbreviation}>
             {item.name}
           </option>
         ))}
+
       </select>
     );
   }
